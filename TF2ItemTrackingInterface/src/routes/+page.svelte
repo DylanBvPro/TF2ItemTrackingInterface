bcc8f46970e7a8154cbf2e9609274f5a8765a87c
Dylan Bv <dylanlbok@gmail.com>
2024-09-09 18:01:57 -0400

    Basic Svelte Introduction
    Ground work for svelte development.

---
+++ TF2ItemTrackingInterface/src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/inventory">Inventory</a>
+        </li>
+    </ul>
+</nav>
+
+
+
+<style>
+    nav {
+      background-color: #333;
+      padding: 1em;
+    }
+  
+    nav ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: space-between;
+    }
+  
+    nav ul li a {
+      color: white;
+      text-decoration: none;
+      padding: 0.5em 1em;
+      display: block;
+    }
+  
+    nav ul li a:hover {
+      background-color: #575757;
+    }
+  </style>
